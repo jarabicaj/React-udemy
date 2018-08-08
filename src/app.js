b29d66ce3e3605054d782ae3e391e4db0d25b4e1
jarabicaj <jarabica.j@gmail.com>
2018-08-08 03:27:09 +0200

    import and export files and modules

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('app.js is running');
+// import subtract, { square, add } from './utils';
+//
+// console.log('app.js is running');
+// console.log(square(3));
+// console.log(add(3, 5));
+// console.log(subtract(100, 5));
+//
+// import isSenior, {isAdult, canDrink} from "./person";
+// const age = 19;
+// console.log(isAdult(age));
+// console.log(canDrink(age));
+// console.log(isSenior(65));
+
+// install -> import -> use
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const template = React.createElement('p', {}, 'testing 123')
+
+ReactDOM.render(template, document.getElementById('app'));
